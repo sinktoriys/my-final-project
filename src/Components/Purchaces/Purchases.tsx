d0278068e3de9c1a934e9ed95cf34d8ceade4857
Marina <m1ivanushkina@gmail.com>
2023-06-07 22:23:59 +0300

    connected Purchases.tsx and ProductListItem.tsx

--- src/Components/Purchaces/Purchases.tsx
+++ src/Components/Purchaces/Purchases.tsx
@@ -1,15 +1,23 @@
 import basket from 'assets/basket.png'
 import user from 'assets/user.png'
-type Props = {}
-const Purchases = (props: Props) => {
+
+type Props = {
+    cartData: {
+        totalCount: number
+        totalPrice: number
+    }
+}
+
+const Purchases = ({ cartData }: Props) => {
     return (
         <div className="purchases">
             <img className="bascket" src={basket} alt="Bascket" />
             <div className="circle">
                 <p className="number" id="products-count">
-                    0
+                    {cartData.totalCount}
                 </p>
             </div>
+            <div className="circle-price">${cartData.totalPrice}</div>
             <img className="user" src={user} alt="User" />
         </div>
     )
--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,13 +1,26 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import Main from 'Container/Main/Main'
+import { StyledEngineProvider } from '@mui/material/styles'
+import { useState } from 'react'
+
+type CartData = {
+    totalCount: number
+    totalPrice: number
+}
+
 const App = () => {
+    const [cartData, setCartData] = useState<CartData>({
+        totalCount: 10,
+        totalPrice: 100,
+    })
+
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header />
+            <Header cartData={cartData} />
             <Main />
-        </>
+        </StyledEngineProvider>
     )
 }
 export default App
