f3ac748b94c2b025a8b44a111b1e999ba921691b
Marina <m1ivanushkina@gmail.com>
2023-07-22 23:02:15 +0300

    created slider

--- src/redux/likeReducer.ts
+++ src/redux/likeReducer.ts
@@ -7,20 +7,6 @@ type LikeState = {
 
 const initialState: LikeState = { }
 
-// export const likeReducer = (state = initialState, action: AnyAction) => {
-//     switch (action.type) {
-//         case 'TOGGLE_LIKE': {
-//             return {
-//                 ...state,
-//                 [action.id]: !state[action.id],
-//             }
-//         }
-//         default:
-//             return state
-//     }
-// }
-
-
 export const likeSlice = createSlice({
     name: 'like',
     initialState,
